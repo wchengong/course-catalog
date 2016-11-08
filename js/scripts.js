3ba90eefbbf830bc5bd34b3c2e4ba7eb6bad20b1
wchengong <wchengong@vcd-ch204-05.eastern.ewu.edu>
2016-11-08 14:33:44 -0800

    version 2
    a lot changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
 // YOUR SCRIPTS
+
+//grid view
+$('#grid').click( function() {
+    //remove only "list"
+    $('.cd-gallery').removeClass('list');
+    //add only "grid"
+    $('.cd-gallery').addClass('grid');
+ 
+});
+
+// list view
+$('#list').click( function() {
+    //remove only "list"
+    $('.cd-gallery').removeClass('grid');
+    //add only "grid"
+    $('.cd-gallery').addClass('list');
+    
+});
